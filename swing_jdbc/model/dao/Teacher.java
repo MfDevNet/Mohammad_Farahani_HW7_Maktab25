6c05c07ddc1d3dacf6fc1e930baa1a9cd4311b58
MfDevNet <MfDevNet@gmail.com>
2019-04-26 00:05:29 +0430

    implement Class Teacher

--- swing_jdbc/model/dao/Teacher.java
+++ swing_jdbc/model/dao/Teacher.java
@@ -1,4 +1,40 @@
 package swing_jdbc.model.dao;
 
 public class Teacher {
+    private int teacherCode;
+    private String firstName;
+    private String lastName;
+
+    public Teacher() {
+    }
+
+    public Teacher(int teacherCode, String firstName, String lastName) {
+        this.teacherCode = teacherCode;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public int getTeacherCode() {
+        return teacherCode;
+    }
+
+    public void setTeacherCode(int teacherCode) {
+        this.teacherCode = teacherCode;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
