44f9a1befe66797bac42dcc8e84b072b6de7d41e
MfDevNet <MfDevNet@gmail.com>
2019-04-26 00:03:06 +0430

    implement Class Student

---
+++ swing_jdbc/model/dao/Student.java
@@ -0,0 +1,40 @@
+package swing_jdbc.model.dao;
+
+public class Student {
+    private int studentCode;
+    private String firstName;
+    private String lastName;
+
+    public Student() {
+    }
+
+    public Student(int studentCode, String firstName, String lastName) {
+        this.studentCode = studentCode;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public int getStudentCode() {
+        return studentCode;
+    }
+
+    public void setStudentCode(int studentCode) {
+        this.studentCode = studentCode;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
