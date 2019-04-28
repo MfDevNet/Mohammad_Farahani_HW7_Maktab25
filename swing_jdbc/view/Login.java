6bbffbbee734e315dcb6f809e29e307f9b6de115
MfDevNet <MfDevNet@gmail.com>
2019-04-28 11:56:31 +0430

    desgin and implelmet login page

---
+++ swing_jdbc/view/Login.java
@@ -0,0 +1,44 @@
+/*
+ * Created by JFormDesigner on Sun Apr 28 11:53:13 IRDT 2019
+ */
+
+package swing_jdbc.view;
+
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.GroupLayout;
+
+/**
+ * @author mohammad
+ */
+public class Login extends JFrame {
+    public Login() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
+        // Generated using JFormDesigner Evaluation license - mohammad
+
+        //======== this ========
+        Container contentPane = getContentPane();
+
+        GroupLayout contentPaneLayout = new GroupLayout(contentPane);
+        contentPane.setLayout(contentPaneLayout);
+        contentPaneLayout.setHorizontalGroup(
+            contentPaneLayout.createParallelGroup()
+                .addGap(0, 400, Short.MAX_VALUE)
+        );
+        contentPaneLayout.setVerticalGroup(
+            contentPaneLayout.createParallelGroup()
+                .addGap(0, 300, Short.MAX_VALUE)
+        );
+        pack();
+        setLocationRelativeTo(getOwner());
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
+    // Generated using JFormDesigner Evaluation license - mohammad
+    // JFormDesigner - End of variables declaration  //GEN-END:variables
+}
